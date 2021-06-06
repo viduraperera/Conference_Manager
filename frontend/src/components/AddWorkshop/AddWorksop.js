9067d77d12e8c29ac6697d20bd4749d647188326
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-06-06 12:50:34 +0530

    some changes in workshop

---
+++ frontend/src/components/AddWorkshop/AddWorksop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {TextField, Button, Typography, Paper, Snackbar} from "@material-ui/core";
+
+const AddWorkshop = () =>{
+    return(
+        <>
+            <Snackbar>
+
+            </Snackbar>
+        </>
+    )
+}
+
+export default AddWorkshop
