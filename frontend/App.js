a5d1b9ddb23b93c4f1e607768f14ef1b187ca17f
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-06-23 17:06:51 +0530

    nav Bar complete

--- frontend/App.js
+++ frontend/App.js
@@ -3,18 +3,14 @@ import {Container} from "@material-ui/core";
 
 import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 
-import NavBar from "./src/components/navBar/navBar";
-import home from "./src/components/home/home";
+import NavBar from "./src/components/navBar/NavBar";
 
 const App = () =>{
     return(
         <BrowserRouter>
-            <Container maxWidth={"lg"}>
-                <NavBar/>
+            <NavBar/>
                 <Switch>
-                    <Route path={"/"} exact component={home}/>
                 </Switch>
-            </Container>
         </BrowserRouter>
     )
 }
