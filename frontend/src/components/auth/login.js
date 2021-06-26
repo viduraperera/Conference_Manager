cfdc5da4307a3687c4bf472575882558f113a01f
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-06-26 22:25:55 +0530

    updated login and register

--- frontend/src/components/auth/login.js
+++ frontend/src/components/auth/login.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
-import { TextField, Typography, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 
 import { login as loginUser } from '../../actions/auth';
 
-const useStyles = makeStyles((theme) => ({
-  field: {
-    marginTop: 20,
-    marginBottom: 20,
-    display: 'block',
-  },
-}));
-
 export default function login() {
   const dispatch = useDispatch();
-  const classes = useStyles();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState(false);
@@ -47,34 +36,35 @@ export default function login() {
       <div className="card" style={{ marginTop: 25 }}>
         <div className="card-body">
           <h2 className="card-title">Login</h2>
-          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-            <TextField
-              className={classes.field}
-              label="Email"
-              variant="outlined"
-              fullWidth
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-                setEmailError(false);
-              }}
-              required
-              error={emailError}
-            />
-            <TextField
-              className={classes.field}
-              label="Password"
-              type="password"
-              variant="outlined"
-              fullWidth
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setPasswordError(false);
-              }}
-              error={passwordError}
-              required
-            />
+          <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                value={email}
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setEmailError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please enter your email.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                required
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setPasswordError(false);
+                }}
+              />
+              {passwordError ? <div className="text-danger">Please enter your password.</div> : ''}
+            </div>
             <button type="submit" className="btn btn-outline-primary">
               Login
             </button>
--- frontend/src/components/auth/register.js
+++ frontend/src/components/auth/register.js
@@ -22,6 +22,8 @@ export default function register() {
     role: '',
   });
 
+  const roleList = ['Researcher', 'Workshop', 'Presenter', 'Attendee'];
+
   const [rePassword, setRePassword] = useState('');
   const [nameError, setNameError] = useState(false);
   const [emailError, setEmailError] = useState(false);
@@ -63,92 +65,97 @@ export default function register() {
 
   return (
     <div className="container">
-      <Typography variant="h6" color="textSecondary">
-        Register
-      </Typography>
-      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <TextField
-          className={classes.field}
-          label="Name"
-          variant="outlined"
-          fullWidth
-          value={user.name}
-          onChange={(e) => {
-            setUser({ ...user, name: e.target.value });
-            setNameError(false);
-          }}
-          required
-          error={nameError}
-        />
-        <TextField
-          className={classes.field}
-          label="Email"
-          variant="outlined"
-          fullWidth
-          value={user.email}
-          onChange={(e) => {
-            setUser({ ...user, email: e.target.value });
-            setEmailError(false);
-          }}
-          required
-          error={emailError}
-        />
-        <TextField
-          className={classes.field}
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          value={user.password}
-          onChange={(e) => {
-            setUser({ ...user, password: e.target.value });
-            setPasswordError(false);
-          }}
-          error={passwordError}
-          required
-        />
-        <TextField
-          className={classes.field}
-          label="Confirm Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          value={rePassword}
-          onChange={(e) => {
-            setRePassword(e.target.value);
-            setPasswordError(false);
-          }}
-          error={passwordError}
-          required
-        />
-        <TextField
-          className={classes.field}
-          label="Contact No"
-          variant="outlined"
-          fullWidth
-          value={user.contactNo}
-          onChange={(e) => {
-            setUser({ ...user, contactNo: e.target.value });
-            setContactNoError(false);
-          }}
-          required
-          error={contactNoError}
-        />
-        <TextField
-          className={classes.field}
-          label="Role"
-          variant="outlined"
-          fullWidth
-          value={user.role}
-          onChange={(e) => {
-            setUser({ ...user, role: e.target.value });
-            setRoleError(false);
-          }}
-          required
-          error={roleError}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <div className="card" style={{ marginTop: 25 }}>
+        <div className="card-body">
+          <h2 className="card-title">Register</h2>
+          <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
+            <div className="col-md-6">
+              <label className="form-label">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={user.name}
+                required
+                onChange={(e) => {
+                  setUser({ ...user, name: e.target.value });
+                  setNameError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please enter your name.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                value={user.email}
+                onChange={(e) => {
+                  setUser({ ...user, email: e.target.value });
+                  setEmailError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please enter your email.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                value={user.password}
+                onChange={(e) => {
+                  setUser({ ...user, password: e.target.value });
+                  setPasswordError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please enter your password.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                value={user.password}
+                value={rePassword}
+                onChange={(e) => {
+                  setRePassword(e.target.value);
+                  setPasswordError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please confirm your password.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Contact Number</label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                value={user.contactNo}
+                onChange={(e) => {
+                  setUser({ ...user, contactNo: e.target.value });
+                  setContactNoError(false);
+                }}
+              />
+              {emailError ? <div className="text-danger">Please enter your contact Number.</div> : ''}
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Register As</label>
+              <select className="form-select" aria-label="Default select example">
+                {roleList.map((role, index) => (
+                  <option value={role} key={index}>
+                    {role}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
