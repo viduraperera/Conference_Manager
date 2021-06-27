71ec9663d9398d3b0c61e3731b4570a903dfa10f
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-06-27 17:54:39 +0530

    fix the footer add work shop card

--- frontend/src/components/home/homePageWorkshop/Workshops.js
+++ frontend/src/components/home/homePageWorkshop/Workshops.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import WorkshopCard from "./WorkShopCard";
+import {getWorkshops} from "../../../actions/workshop"
+
+import {useDispatch, useSelector} from "react-redux";
 
 const Workshop = () =>{
+
+        const dispatch = useDispatch();
+
+        useEffect( ()=>{
+            dispatch(getWorkshops());
+
+        }, [dispatch]);
+
+        const workshops = useSelector( (state) => state.workshop.workshops);
+        console.log(workshops);
+
     return(
-        <WorkshopCard/>
-    )
+            <div className={"row container-lg mx-auto mt-5 px-0"}>
+                <h1 className="display-5 text-center mb-5">Workshops</h1>
+                <div>
+                    {workshops?.map((workshop) =>{
+                        return <WorkshopCard workshop={workshop} key={workshop._id}/>
+                    })}
+                </div>
+            </div>
+    );
 }
 
 export  default Workshop;
